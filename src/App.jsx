4c10893b8d561512ff6db3381cb04dc9cb40c2d3
Jason Rojas <jasonmix84@gmail.com>
2025-04-19 14:01:58 -0400

    Finally got the gemini api working. I will try to make the output look better. 1 bug I have is I need to do 2 confirms so i need to ensure that works properly. THen I need to also deploy but might not be able to do that yet.

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,60 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+import { GoogleGenAI } from "@google/genai"
+const apiKey = import.meta.env.VITE_API_KEY;
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [text, setText] = useState("");
+  const [fullText, setFullText] = useState("");
+  const [AIResponse, setAIResponse] = useState("Please try again");
+  console.log("starting")
+  const ai = new GoogleGenAI( { apiKey: apiKey});
+
+  async function sendToGemini(text) {
+    console.log("in the async function send to gemini")
+    const string1 = "This is text from a website. I need you to write bullet points about the main points and where you got them from.";
+    const contentsText = string1.concat(" ", text);
+    console.log("the prompt is ", contentsText)
+    const response = await ai.models.generateContent({
+      model: "gemini-2.0-flash",
+      contents: contentsText,
+      config: {
+        temperature: 0.5,
+        maxOutputTokens: 500,
+      }
+    });
+    console.log("response is ", String(response.text))
+    setAIResponse(String(response.text));
+
+  };
+
+  const handleChange = (event) => {
+    console.log("inside the handle change")
+    setText(event.target.value);
+  };
+  async function handleConfirm(){
+    console.log("within the send to gemini")
+    setFullText(text)
+    console.log("The full text is", fullText)
+    await sendToGemini(fullText);
+  };
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <label htmlFor="my-input">Enter text:</label>
+      <input
+        type="text"
+        id="my-input"
+        value={text}
+        onChange={handleChange}
+      />
+      <button onClick={handleConfirm}>Confirm</button>
+      {AIResponse && <p>Your Summary:<br></br> {AIResponse}</p>}
+    </div>
     </>
   )
 }
